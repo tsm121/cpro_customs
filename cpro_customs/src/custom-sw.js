33b332bb066dae866b1015598bd6f959e554217b
Thomas M <tsm121@gmail.com>
2018-09-30 16:11:45 +0200

    Added manifest.json to cache

--- cpro_customs/src/custom-sw.js
+++ cpro_customs/src/custom-sw.js
@@ -1,3 +1,6 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+
 // See https://developers.google.com/web/tools/workbox/guides/configure-workbox
 workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
@@ -5,7 +8,15 @@ self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 
 // We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
+//workbox.precaching.precacheAndRoute(self.__precacheManifest);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 // app-shell
-workbox.routing.registerRoute("/", workbox.strategies.networkFirst());
+workbox.routing.registerRoute("/manifest.json", workbox.strategies.staleWhileRevalidate({
+	cacheName: 'STATIC'
+}));
+
+workbox.routing.registerRoute("/custom-sw.js", workbox.strategies.staleWhileRevalidate({
+	cacheName: 'STATIC'
+}));
