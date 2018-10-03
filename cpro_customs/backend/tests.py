6a0c2eb4c1923e1cb09711c42942338f57b19e7a
Manuel Goester <manueljg@ntnu.no>
2018-10-03 14:23:48 +0200

    Added a dummy test case

--- cpro_customs/backend/tests.py
+++ cpro_customs/backend/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class DummyTestCase(TestCase):
+    def setUp(self):
+        print("setUp called")
+
+    def test_dummy(self):
+        self.assertEqual("", "")
