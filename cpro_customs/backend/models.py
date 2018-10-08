33d59faa11faa1b820f3ba7b851064a24cb8432c
Alexander Marchand-Melsom <alexamar@stud.ntnu.no>
2018-10-08 22:09:53 +0200

    Implement the data model in the database
    Followed the data model and create the models needed to create the
    structure of the database.

--- cpro_customs/backend/models.py
+++ cpro_customs/backend/models.py
@@ -2,10 +2,28 @@ from django.db import models
 
 # Create your models here.
 
-class LicensePlate(models.Model):
-    number = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    year_of_issue = models.IntegerField(default=0)
+class Transaction(models.Model):
+    license_plate = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    taxes_and_fees = models.FloatField(default=0)
     
     def __str__(self):
         return self.text
+
+class Product(models.Model):
+    type_of_product = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+
+class TransactionProduct(models.Model):
+    transaction_ID = models.ForeignKey(Transaction, on_delete=models.PROTECT)
+    product_ID = models.ForeignKey(Product, on_delete=models.PROTECT)
+    price = models.FloatField(default=0)
+    weight = models.FloatField(default=None)
+    volume = models.FloatField(default=None)
+    pieces = models.IntegerField(default=None)
+
+    def __str__(self):
+        return self.text
+
