52268cf58e16ca88ee18290c5b31f78a8a64fc58
Alexander Marchand-Melsom <alexamar@stud.ntnu.no>
2018-10-08 03:17:17 +0200

    Configure PSQL for Django
    The PSQL database allows us to store information in a more permanent
    way, as well as emulate access to the customer's database. It is
    configured through the backend/models.py file, andaccessed through
    settings.py, which also serves as a proxy to access thedatabase
    password contained in secrets.json (not commited for obvious
    reasons).

--- cpro_customs/backend/models.py
+++ cpro_customs/backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class LicensePlate(models.Model):
+    number = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    year_of_issue = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.text
