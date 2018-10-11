4a61607ffc5c2897673548f880aff1621bde6478
Manuel Goester <manueljg@ntnu.no>
2018-10-11 13:03:59 +0200

    Merged Router.js

--- cpro_customs/src/components/Router.js
+++ cpro_customs/src/components/Router.js
@@ -16,16 +16,11 @@ class Router extends Component {
     render() {
 		return (
             <Switch>
-<<<<<<< HEAD
-				<Route exact path='/' component={Home}/>
-				<Route exact path='/persons-in-vehicle' component={PersonsInVehicle}/>
-				<Route exact path='/on-boarding' component={OnBoarding}/>
-=======
-              <Route exact path='/' component={Home}/>
-              <Route exact path='/persons-in-vehicle' component={PersonsInVehicle}/>
-              <Route exact path='/categories' component={Overview}/>
-              <Route exact path='/categories/animals' component={Animals}/>
->>>>>>> frontend_design_#32
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/persons-in-vehicle' component={PersonsInVehicle}/>
+                <Route exact path='/on-boarding' component={OnBoarding}/>
+                <Route exact path='/categories' component={Overview}/>
+                <Route exact path='/categories/animals' component={Animals}/>
             </Switch>
 		);
 	}
