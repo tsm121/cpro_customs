c512fc0ac21d995db9bd23097157e792ae38e60f
Manuel Goester <manueljg@ntnu.no>
2018-10-15 12:06:09 +0200

    Changed grid to use only xs sm and md

--- cpro_customs/src/components/pages/categories/animals/Animals.js
+++ cpro_customs/src/components/pages/categories/animals/Animals.js
@@ -16,48 +16,34 @@ class Animals extends Component {
                       spacing={0}
                       direction={"row"}
                 >
-                    <Grid item xs={12} sm={12} md={1} lg={1} xl={1}>
+                    <Grid item xs={11} sm={10} md={8}>
                         <Paper className={"cdp_paper_category_sub_selection"}>
                             <Grid container
                                   justify={"flex-start"}
                                   alignItems={"center"}
                                   spacing={0}
                             >
-                                <Grid item xl={2}>
+                                <Grid item xs={3} sm={3} md={3}>
                                     <Grid container
                                           justify={"flex-end"}
                                           alignItems={"center"}
                                     >
-                                        <Grid item xl={6}>
+                                        <Grid item xs={8} sm={8} md={8}>
                                             <img className={"icon_md"}
                                                  src={require("assets/img/icons/512x512/dog_dark_grey.png")}
                                                  alt={"icon"}
                                             />
                                         </Grid>
-                                        <Grid item xl={3}>
+                                        <Grid item xs={2} sm={3} md={2}>
                                         </Grid>
                                     </Grid>
                                 </Grid>
-                                <Grid item xl={10}>
+                                <Grid item xs={9} sm={9} md={9}>
                                     <span className={"cdp_category_sub_selection cdp_dark_grey"}>
                                         I am travelling with my pet
                                     </span>
                                 </Grid>
                             </Grid>
-                        </Paper>
-                    </Grid>
-                </Grid>
-                <Grid container
-                      justify={"center"}
-                      alignItems={"center"}
-                      spacing={0}
-                      direction={"row"}
-                >
-                    <Grid item xs={11} sm={10} md={9} lg={8} xl={8}>
-                        <Paper>
-                            <h1 className={"cdp cdp_secondary"}>
-                                Test
-                            </h1>
                         </Paper>
                     </Grid>
                 </Grid>
