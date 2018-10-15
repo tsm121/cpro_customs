624691dcd585ddea33ff3a29b9d1b584e025947d
Thomas M <tsm121@gmail.com>
2018-10-15 19:05:20 +0200

    Fixed formatting for mobile units
    Related to issue #32

--- cpro_customs/src/components/header/LangButton.js
+++ cpro_customs/src/components/header/LangButton.js
@@ -4,57 +4,40 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
-
-const styles =({
-	flag_img: {
-		height:'70px',
-		width:'70px',
-		margin: '10px'
-	},
-
-	flag_name: {
-		textAlign: 'center',
-		margin:'0px',
-	},
-
-	flag_button: {
-
-		borderRadius: '0',
-		WebkitBoxShadow:'inset 0 0 5px #7d8f9e',
-		MozBoxShadow:'inset 0 0 5px #7d8f9e',
-		boxShadow:'inset 0 0 5px #7d8f9e',
-		margin: '10px'
-	}
-});
-
-
 export default class LangButton extends Component  {
 	render = () => {
 		const {text, countryName} = this.props
 
 		return (
-			<div>
-				<Button size={"large"}
-						style={styles.flag_button}
+			<Button
+				className={"flag_button"}
+			>
+				<Grid container
+					  direction={"column"}
+					  justify={"center"}
+					  alignItems={"center"}
 				>
-					<Grid container
-						  spacing={8}
-						  direction={"column"}
+					<Grid item className={"flag_container"}
+						  xs={12}
+						  sm={12}
+						  md={12}
 					>
-						<Grid item>
-							<Avatar
-								src={require(`assets/header/lang_icons/${countryName}.png`)}
-								style={styles.flag_img}
-							/>
-						</Grid>
-						<Grid item>
-							<h3 style={styles.flag_name}>
-								{text}
-							</h3>
-						</Grid>
+						<Avatar
+							src={require(`assets/header/lang_icons/${countryName}.png`)}
+							className={"flag_img"}
+						/>
 					</Grid>
-				</Button>
-			</div>
+					<Grid item
+						  xs={12}
+						  sm={12}
+						  md={12}
+					>
+						<h3 className={"flag_name"}>
+							{text}
+						</h3>
+					</Grid>
+				</Grid>
+			</Button>
 		)
 	}
 }
