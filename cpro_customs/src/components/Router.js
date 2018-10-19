a5f0463a2a918be8a4e2e22f75930fec13592ef7
Manuel Goester <manueljg@ntnu.no>
2018-10-19 10:45:48 +0200

    aligned Router.js code

--- cpro_customs/src/components/Router.js
+++ cpro_customs/src/components/Router.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom'
 
 import Home from './pages/home/Home'
 import PersonsInVehicle from './pages/persons-in-vehicle/PersonsInVehicle'
@@ -22,25 +22,25 @@ import Endpage from "./pages/endpage/Endpage";
  * How routing works: https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
  */
 class Router extends Component {
-	render() {
-		return (
-			<Switch>
-				<Route exact path='/' component={Home}/>
-				<Route exact path='/persons-in-vehicle' component={PersonsInVehicle}/>
-				<Route exact path='/on-boarding' component={OnBoarding}/>
-				<Route exact path='/checkout' component={Checkout}/>
-				<Route exact path='/endpage' component={Endpage}/>
-				<Route exact path='/categories' component={Categories} />
-        <Route exact path='/categories/animals' component={Animals}/>
-        <Route exact path='/categories/animals/pet' component={Pet}/>
-        <Route exact path='/categories/animals/horse' component={Horse}/>
-        <Route exact path='/categories/animals/import' component={Import}/>
-        <Route exact path='/categories/animals/bought' component={Bought}/>
-        <Route exact path='/categories/goods' component={Goods}/>
-				<Route path="*" component={NotFound} />
-			</Switch>
-		);
-	}
+    render() {
+        return (
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/persons-in-vehicle' component={PersonsInVehicle}/>
+                <Route exact path='/on-boarding' component={OnBoarding}/>
+                <Route exact path='/checkout' component={Checkout}/>
+                <Route exact path='/endpage' component={Endpage}/>
+                <Route exact path='/categories' component={Categories}/>
+                <Route exact path='/categories/animals' component={Animals}/>
+                <Route exact path='/categories/animals/pet' component={Pet}/>
+                <Route exact path='/categories/animals/horse' component={Horse}/>
+                <Route exact path='/categories/animals/import' component={Import}/>
+                <Route exact path='/categories/animals/bought' component={Bought}/>
+                <Route exact path='/categories/goods' component={Goods}/>
+                <Route path="*" component={NotFound}/>
+            </Switch>
+        );
+    }
 }
 
 export default Router;
