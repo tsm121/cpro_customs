d631fa4eb3997303cbf84fd46cdbb36f807f86f8
Alexander Marchand-Melsom <alexamar@stud.ntnu.no>
2018-10-21 16:49:36 +0200

    Add BasicAuthentication to the API

--- cpro_customs/backend/views.py
+++ cpro_customs/backend/views.py
@@ -1,54 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from backend.models import Transaction
 from backend.serializers import TransactionSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-#TODO: DELETE THIS LINE --> SECURITY
-@csrf_exempt
-def transaction_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
+class TransactionList(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
         transactions = Transaction.objects.all()
         serializer = TransactionSerializer(transactions, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = TransactionSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = TransactionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#TODO: DELETE THIS LINE --> SECURITY
-@csrf_exempt
-def transaction_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        transaction = Transaction.objects.get(pk=pk)
-    except Transaction.DoesNotExist:
-        return HttpResponse(status=404)
-    
-    if request.method == 'GET':
-        serializer = TransactionSerializer(transaction)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = TransactionSerializer(transaction, data=data)
-        if serializer.is_valid():
-            serializer.save()
+'''
+class TransactionDetail(APIView):
+    def get(self, request, pk):
+        try:
+            transaction = Transaction.objects.get(pk=pk)
+        except Transaction.DoesNotExist:
+            return HttpResponse(status=404)
+        
+        if request.method == 'GET':
+            serializer = TransactionSerializer(transaction)
             return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
 
-    elif request.method == 'DELETE':
-        transaction.delete()
-        return HttpResponse(status=204)
+        elif request.method == 'PUT':
+            data = JSONParser().parse(request)
+            serializer = TransactionSerializer(transaction, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors, status=400)
 
+        elif request.method == 'DELETE':
+            transaction.delete()
+            return HttpResponse(status=204)
+'''
