8fbb19e9097a1acfbe3d391ea08cd4236663c9b4
Manuel Goester <manueljg@ntnu.no>
2018-10-30 14:38:15 +0100

    Add functions to add/remove a product to/from products

--- cpro_customs/src/components/App.js
+++ cpro_customs/src/components/App.js
@@ -1,21 +1,12 @@
 import React, {Component} from 'react';
 import {MuiThemeProvider} from '@material-ui/core/styles';
-//import { withStyles } from '@material-ui/core/styles';
 
 import theme from '../material-theme'
-
 import {GlobalState} from "./global_state/GlobalState";
-import './App.css';
 import Header from './header/Header'
 import Router from './Router'
+import './App.css';
 
-
-let productId = 0;
-
-function createRows(category, filename, amount, unit, value, vat, duty) {
-    productId++;
-    return {id: productId, category, filename, amount, unit, value, vat, duty};
-}
 
 /**
  * The entry point to the SPA
@@ -26,28 +17,57 @@ class App extends Component {
         this.state = {
             products: [],
             amount_to_pay: 0,
+            productIdCounter: 0,
         };
     }
 
-    onAddToCart = this.onAddToCart.bind(this);
-    onAddToCart(p) {
-        p['id'] = productId++;
+    /*
+     * bind the functions
+     * TODO: This is necessary, but why?
+     */
+    addProduct = this.addProduct.bind(this);
+    removeProduct = this.removeProduct.bind(this);
+    addGood = this.addGood.bind(this);
+    addAlcohol = this.addAlcohol.bind(this);
+    getAlcohol = this.getAlcohol.bind(this);
+
+    /**
+     * Adds a product to products
+     * @param product - the product to be added
+     */
+    addProduct(product) {
+        product['id'] =  this.state.productIdCounter;
         this.setState({
-            products: [...this.state.products, p]
+            productIdCounter: product['id'] + 1,
+            products: [...this.state.products, product]
         });
     }
 
-    removeProductFromCart = this.removeProductFromCart.bind(this);
-    removeProductFromCart(id) {
-        const {products} = this.state;
-        // TODO: delete product with this id from products
+    /**
+     * Searches for product with id and removes it from products
+     * @param id - the id of the product
+     */
+    removeProduct(id) {
+        let {products} = this.state;
+        for (let i = 0; i < products.length; ++i) {
+            if (products[i].id === id) {
+                products.splice(i, 1);
+                break;
+            }
+        }
         this.setState({
-            items: products
+            items: products,
         });
     }
 
-    addGoodToCart = this.addGoodToCart.bind(this);
-    addGoodToCart(name, value, currency, amount) {
+    /**
+     * Adds a Good to products
+     * @param name - the name of the Good, e.g. "kitchen"
+     * @param value - the value of the Good, e.g. 30000
+     * @param currency - the currency of the value, e.g. "NOK"
+     * @param amount - amount of items, e.g. 1
+     */
+    addGood(name, value, currency, amount) {
         let good = {
             "product": "Goods",
             "name": name,
@@ -58,8 +78,13 @@ class App extends Component {
         this.onAddToCart(good);
     }
 
-    addAlcoholToCart = this.addAlcoholToCart.bind(this);
-    addAlcoholToCart(type, value, amount) {
+    /**
+     * Adds alcohol of specific type to products
+     * @param type - type of alcohol, e.g. "lightBeer"
+     * @param value - how many litres, e.g. 0.5
+     * @param amount - amount of items, e.g. 6
+     */
+    addAlcohol(type, value, amount) {
         let item = {
             "product": type,
             "value": value,
@@ -71,9 +96,11 @@ class App extends Component {
 
     /**
      * Searches for an alcohol item in the global state
+     * @param category - type of alcohol, e.g. "lightBeer"
+     * @param value - how many litres, e.g. 0.5
+     * @param pitcher - boolean, true if its a pitcher item
      */
-    getAlcoholFromCart = this.getAlcoholFromCart.bind(this);
-    getAlcoholFromCart(category, value, pitcher) {
+    getAlcohol(category, value, pitcher) {
         const {products} = this.state;
         if (pitcher) {
             products.forEach((item) => {
@@ -90,7 +117,6 @@ class App extends Component {
         }
         return null;
     }
-
 
     render() {
         return (
@@ -117,5 +143,4 @@ class App extends Component {
     }
 }
 
-//export default withStyles()(App);
 export default (App);
