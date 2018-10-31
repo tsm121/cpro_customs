733b26c3402061b7a1bd9189d5f7b478b94b4429
tsm121 <tsm121@gmail.com>
2018-10-31 18:39:22 +0100

    Added input for abroad more than 24 hours
    Related to issue #132

--- cpro_customs/src/components/pages/persons-in-vehicle/PersonsInVehicle.js
+++ cpro_customs/src/components/pages/persons-in-vehicle/PersonsInVehicle.js
@@ -7,159 +7,216 @@ import Paper from '@material-ui/core/Paper';
 import '../../App.css' // TODO: make this path absolute
 import './PersonsInVehicle.css'
 import NavigationArrow from '../../NavigationArrow'
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import HelpTip from "../../HelpTip";
+import {TOOL_TIP_TEXTS} from "../../../data/ToolTipTexts";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 
-class PersonsInVehicle extends Component {
-	constructor(props) {
-		super(props);
-		// init state
-		this.state = {
-			showYellowIcon: [true, false, false, false, false, false, false],
-			iconClicked: [false, false, false, false, false, false, false],
-			amountPersonsShown: 5,
-			plusClickCounter: 0,
-		};
-	}
 
-	render() {
-		const {showYellowIcon, amountPersonsShown} = this.state;
-		return (
-			<div>
-				<Grid container
-					  spacing={0}
-					  justify="center"
-					  alignItems="center"
-					  direction="column"
-				>
-					<Grid item xl={12}>
-						<h1 className={"cdp cdp_primary"}>For how many people do you want to declare goods?</h1>
-					</Grid>
-
-					<Grid container
-						  spacing={0}
-						  justify="center"
-						  alignItems="center"
-						  direction="row"
-						  className="personContainer"
-					>
-						<Grid item
-							  xs={11}
-							  sm={8}
-							  md={7}
-							  xl={6}
-						>
-							<Paper className="personPaper">
-								<Grid container justify="center" spacing={0}>
-									{ /* person icons */
-										(Array.apply(null, {length: amountPersonsShown}).map(Number.call, Number))
-											.map(value => (
-												<Grid item key={value}
-													  onMouseEnter={() => this.personOnMouseOver(value)}
-													  onMouseLeave={() => this.personOnMouseOut(value)}
-													  onClick={() => this.personOnClick(value)}
-												>
-													<img className="icon_sm" src={showYellowIcon[value] ?
-														require(`assets/img/icons/128x128/person_yellow.png`) :
-														require(`assets/img/icons/128x128/person_black.png`)}
-														 alt="icon"
-													/>
-												</Grid>
-											))
-									}
-									{ /* plus button */
-										amountPersonsShown < 12
-											?   <Grid item
-													  onClick={() => this.plusOnClick()}
-													  onMouseOver={() => this.plusOnMouseOver()}
-													  onMouseOut={() => this.plusOnMouseOut()}
-											>
-												<img className="icon_sm"
-													 src={require(`assets/img/icons/128x128/plus_black.png`)}
-													 alt="icon"
-												/>
-											</Grid>
-											: null
-									}
-								</Grid>
-							</Paper>
-						</Grid>
-					</Grid>
-					<NavigationArrow direction={"right"} page={"categories"}/>
-				</Grid>
-			</div>
-		);
-	}
-
-	plusOnClick() {
-		this.setState({
-			showYellowIcon: update(this.state.showYellowIcon, {$push: [false]}),
-			iconClicked: update(this.state.iconClicked, {$push: [false]}),
-			amountPersonsShown: this.state.amountPersonsShown + 1,
-			plusClickCounter: this.state.plusClickCounter + 1,
-		});
-	}
-
-	plusOnMouseOver() {
-		document.body.style.cursor = "pointer";
-	}
-
-	plusOnMouseOut() {
-		document.body.style.cursor = "default";
-	}
-
-	/**
-	 * Event handler for mouse over
-	 * Updates the state -> yellow icons are shown
-	 * @param id - the id of the icon
-	 */
-	personOnMouseOver(id) {
-		const {showYellowIcon, iconClicked, amountPersonsShown} = this.state;
-		if (iconClicked[id]) {
-			return;
-		}
-		document.body.style.cursor = "pointer";
-		let showYellowIcon_2 = showYellowIcon;
-		for (let i=0; i <= id; i++) showYellowIcon_2[i] = true;
-		for (let i = id + 1; i < amountPersonsShown; i++) showYellowIcon_2[i] = false;
-		this.setState({
-			showYellowIcon: update(showYellowIcon_2, {$set: showYellowIcon_2})
-		});
-	}
-
-	/**
-	 * Event handler for mouse out
-	 * Updates the state -> black icons are shown
-	 * @param id - the id of the icon
-	 */
-	personOnMouseOut(id) {
-		const {showYellowIcon, iconClicked, amountPersonsShown} = this.state;
-
-		if (iconClicked[id]) {
-			this.setState({
-				iconClicked: update(iconClicked, {[id]: {$set: false}})
-			});
-			return;
-		}
-		if (id === 0) return;
-		document.body.style.cursor = "default";
-		let showYellowIcons_2 = showYellowIcon;
-		for (let i=1; i < amountPersonsShown; i++) showYellowIcons_2[i] = false;
-		this.setState({
-			showYellowIcon: update(showYellowIcon, {$set: showYellowIcons_2})
-		});
-	}
-
-	/**
-	 * Event handler for on click
-	 * Updates the state -> marks current icon as clicked
-	 * @param id - the id of the icon
-	 */
-	personOnClick(id) {
-		const {iconClicked} = this.state;
-		this.setState({
-			iconClicked: update(iconClicked, {[id]: {$set: true}})
-		});
-	}
+const styles = {
+    checkbox_label: {
+        color: "white",
+        fontSize: "4vmin",
+    },
 }
 
-export default PersonsInVehicle;
+class PersonsInVehicle extends Component {
+    constructor(props) {
+        super(props);
+        // init state
+        this.state = {
+            showYellowIcon: [true, false, false, false, false, false, false],
+            iconClicked: [false, false, false, false, false, false, false],
+            amountPersonsShown: 5,
+            plusClickCounter: 0,
+            overADay: false,
+        };
+    }
+
+    handleChecked = event =>{
+        this.setState({
+            overADay: event.target.checked
+        })
+
+    }
+
+    render() {
+        const {showYellowIcon, amountPersonsShown, overADay} = this.state;
+        const {classes} = this.props
+        return (
+            <div>
+                <Grid container
+                      spacing={0}
+                      justify="center"
+                      alignItems="center"
+                      direction="column"
+                >
+                    <Grid item xl={12}>
+                        <h1 className={"cdp cdp_primary"}>For how many people do you want to declare?</h1>
+                    </Grid>
+
+                    <Grid container
+                          spacing={0}
+                          justify="center"
+                          alignItems="center"
+                          direction="row"
+                          className="personContainer"
+                    >
+                        <Grid item
+                              xs={11}
+                              sm={8}
+                              md={7}
+                              xl={6}
+                        >
+                            <Paper className="personPaper">
+                                <Grid container justify="center" spacing={0}>
+                                    { /* person icons */
+                                        (Array.apply(null, {length: amountPersonsShown}).map(Number.call, Number))
+                                            .map(value => (
+                                                <Grid item key={value}
+                                                      onMouseEnter={() => this.personOnMouseOver(value)}
+                                                      onMouseLeave={() => this.personOnMouseOut(value)}
+                                                      onClick={() => this.personOnClick(value)}
+                                                >
+                                                    <img className="icon_sm" src={showYellowIcon[value] ?
+                                                        require(`assets/img/icons/128x128/person_yellow.png`) :
+                                                        require(`assets/img/icons/128x128/person_black.png`)}
+                                                         alt="icon"
+                                                    />
+                                                </Grid>
+                                            ))
+                                    }
+                                    { /* plus button */
+                                        amountPersonsShown < 12
+                                            ?   <Grid item
+                                                      onClick={() => this.plusOnClick()}
+                                                      onMouseOver={() => this.plusOnMouseOver()}
+                                                      onMouseOut={() => this.plusOnMouseOut()}
+                                            >
+                                                <img className="icon_sm"
+                                                     src={require(`assets/img/icons/128x128/plus_black.png`)}
+                                                     alt="icon"
+                                                />
+                                            </Grid>
+                                            : null
+                                    }
+                                </Grid>
+                            </Paper>
+                        </Grid>
+
+                        <Grid container
+                                  direction={"row"}
+                                  alignItems={"center"}
+                                  justify={"center"}
+                            >
+                                <Grid item
+                                      className={"checkbox_container"}
+                                >
+                                    <FormControlLabel
+                                        control={
+                                            <Checkbox
+                                                checked={overADay}
+                                                onChange={this.handleChecked}
+                                                value="overADay"
+                                                color="secondary"
+                                            />
+                                        }
+                                        label=' "I have been abroad more than 24 hours" '
+
+                                        classes={{
+                                            label: classes.checkbox_label,
+                                        }}
+                                        className={"checkbox_label"}
+                                    />
+
+                                </Grid>
+
+                                <Grid item style={{marginTop:"35px"}}
+                                >
+                                    <HelpTip text={TOOL_TIP_TEXTS.personsInVehicle.abroad} placement={"bottom"} light={true}/>
+                                </Grid>
+
+                            </Grid>
+
+                    </Grid>
+                    <NavigationArrow direction={"right"} page={"categories"}/>
+                </Grid>
+            </div>
+        );
+    }
+
+    plusOnClick() {
+        this.setState({
+            showYellowIcon: update(this.state.showYellowIcon, {$push: [false]}),
+            iconClicked: update(this.state.iconClicked, {$push: [false]}),
+            amountPersonsShown: this.state.amountPersonsShown + 1,
+            plusClickCounter: this.state.plusClickCounter + 1,
+        });
+    }
+
+    plusOnMouseOver() {
+        document.body.style.cursor = "pointer";
+    }
+
+    plusOnMouseOut() {
+        document.body.style.cursor = "default";
+    }
+
+    /**
+     * Event handler for mouse over
+     * Updates the state -> yellow icons are shown
+     * @param id - the id of the icon
+     */
+    personOnMouseOver(id) {
+        const {showYellowIcon, iconClicked, amountPersonsShown} = this.state;
+        if (iconClicked[id]) {
+            return;
+        }
+        document.body.style.cursor = "pointer";
+        let showYellowIcon_2 = showYellowIcon;
+        for (let i=0; i <= id; i++) showYellowIcon_2[i] = true;
+        for (let i = id + 1; i < amountPersonsShown; i++) showYellowIcon_2[i] = false;
+        this.setState({
+            showYellowIcon: update(showYellowIcon_2, {$set: showYellowIcon_2})
+        });
+    }
+
+    /**
+     * Event handler for mouse out
+     * Updates the state -> black icons are shown
+     * @param id - the id of the icon
+     */
+    personOnMouseOut(id) {
+        const {showYellowIcon, iconClicked, amountPersonsShown} = this.state;
+
+        if (iconClicked[id]) {
+            this.setState({
+                iconClicked: update(iconClicked, {[id]: {$set: false}})
+            });
+            return;
+        }
+        if (id === 0) return;
+        document.body.style.cursor = "default";
+        let showYellowIcons_2 = showYellowIcon;
+        for (let i=1; i < amountPersonsShown; i++) showYellowIcons_2[i] = false;
+        this.setState({
+            showYellowIcon: update(showYellowIcon, {$set: showYellowIcons_2})
+        });
+    }
+
+    /**
+     * Event handler for on click
+     * Updates the state -> marks current icon as clicked
+     * @param id - the id of the icon
+     */
+    personOnClick(id) {
+        const {iconClicked} = this.state;
+        this.setState({
+            iconClicked: update(iconClicked, {[id]: {$set: true}})
+        });
+    }
+}
+
+export default withStyles(styles)(PersonsInVehicle);
