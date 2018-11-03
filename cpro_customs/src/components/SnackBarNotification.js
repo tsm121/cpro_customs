03c911735376047e8fa2c05d689dcf838a2d3aa2
Manuel Goester <manueljg@ntnu.no>
2018-11-03 10:03:59 +0100

    Change style of snackbar to white with dark grey text

--- cpro_customs/src/components/SnackBarNotification.js
+++ cpro_customs/src/components/SnackBarNotification.js
@@ -1,9 +1,47 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
+import {withStyles} from '@material-ui/core/styles';
 import IconButton from "@material-ui/core/IconButton/IconButton";
 import CloseIcon from '@material-ui/icons/Close';
 import Snackbar from "@material-ui/core/Snackbar/Snackbar";
+import SnackbarContent from "@material-ui/core/SnackbarContent/SnackbarContent";
+
+const styles1 = theme => ({
+    snackbar: {
+        backgroundColor: "white",
+    },
+});
+
+function MySnackbarContent(props) {
+    const {classes, className, message, onExited, ...other} = props;
+
+    return (
+        <SnackbarContent
+            className={classNames(classes["snackbar"], className)}
+            aria-describedby="client-snackbar"
+            message={
+                <span id="client-snackbar" className={"cdp_dark_grey"}>
+                    {message}
+                </span>
+            }
+            action={[
+                <IconButton
+                    key="close"
+                    aria-label="Close"
+                    color="black"
+                    onClick={onExited}
+                >
+                    <CloseIcon/>
+                </IconButton>,
+            ]}
+            {...other}
+        />
+    );
+}
+
+const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
 
 class SnackBarNotification extends Component {
 
@@ -22,18 +60,13 @@ class SnackBarNotification extends Component {
                     ContentProps={{
                         'aria-describedby': 'message-id',
                     }}
-                    message={<span id="message-id">{this.props.message}</span>}
-                    action={[
-                        <IconButton
-                            key="close"
-                            aria-label="Close"
-                            color="inherit"
-                            onClick={this.props.onExited}
-                        >
-                            <CloseIcon/>
-                        </IconButton>,
-                    ]}
-                />
+                >
+                    <MySnackbarContentWrapper
+                        onClose={this.props.onClose}
+                        onExited={this.props.onExited}
+                        message={this.props.message}
+                    />
+                </Snackbar>
             </div>
         );
     };
