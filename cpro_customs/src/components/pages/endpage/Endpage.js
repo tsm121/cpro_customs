1b15770f0441561c976c7a6d46268eb00c7de4c7
tsm121 <tsm121@gmail.com>
2018-11-06 17:26:24 +0100

    Added generation of QR code at end page
    Still need to pass the QR_url from the payment component as a prop after fetching from server.
    
    Related to issue #37

--- cpro_customs/src/components/pages/endpage/Endpage.js
+++ cpro_customs/src/components/pages/endpage/Endpage.js
@@ -5,52 +5,56 @@ import FormControl from "@material-ui/core/FormControl/FormControl";
 import Receipt from "./Receipt";
 
 export default class Endpage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			//TODO: This should be sent as a prop from payment component
-			QR_url: require("assets/img/receipts/QR_code.png"),
-		}
+    constructor(props) {
+        super(props);
 
-	}
+    }
 
-	render = () => {
-		const {QR_url} = this.state
+    render = () => {
+        //TODO: Uncomment this when QR_url gets sent as a prop from checkout
+        //const {QR_url} = this.props
+        //TODO: Remove this when sending QR_url from checkout
+        const QR_url = "http://google.com"
 
-		let receipt_block
 
-		if(QR_url.length >= 0){
-			receipt_block = <Grid item
-			>
-				<Receipt QR_url={QR_url}/>
-			</Grid>
-		} else {
-			receipt_block = ""
-		}
-		return(
+        let receipt_block
 
-			<FormControl fullWidth={true}>
-				<Grid container
-					  spacing={0}
-					  justify="center"
-					  alignItems="center"
-					  direction="column"
-				>
-					<Grid item>
-						<h1 className={"h1Style"}>
-							Thank you for your time.
-						</h1>
-					</Grid>
-					<Grid item
-					>
-						<h1 className={"h1Style_secondary_big"}>
-							Welcome to Norway!
-						</h1>
-					</Grid>
+        if (QR_url !== undefined) {
 
-					{receipt_block}
-				</Grid>
-			</FormControl>
-		)
-	}
+            if(QR_url.length >= 0){
+                receipt_block = <Grid item
+                >
+                    <Receipt QR_url={QR_url}/>
+                </Grid>
+            }
+
+        } else {
+            receipt_block = ""
+        }
+        return(
+
+            <FormControl fullWidth={true}>
+                <Grid container
+                      spacing={0}
+                      justify="center"
+                      alignItems="center"
+                      direction="column"
+                >
+                    <Grid item>
+                        <h1 className={"h1Style"}>
+                            Thank you for your time.
+                        </h1>
+                    </Grid>
+                    <Grid item
+                    >
+                        <h1 className={"h1Style_secondary_big"}>
+                            Welcome to Norway!
+                        </h1>
+                    </Grid>
+
+                    {receipt_block}
+                </Grid>
+            </FormControl>
+        )
+    }
 }
