1a8cd214733cc5bcf75121f1cdc0bdf353f25aec
tsm121 <tsm121@gmail.com>
2018-11-11 18:28:03 +0100

    Forgot to add some files to last commit

--- cpro_customs/workbox-config.js
+++ cpro_customs/workbox-config.js
@@ -1,19 +1,26 @@
 module.exports = {
-	//Global directory of app
-	"globDirectory": ".",
-	//Patterns for files that will be added
-	"globPatterns": [
-		//Add all assets images
-		"public/assets/**/*.{png,jpg,svg}",
-		"public/manifest.json"
-	],
-	//Destination to service worker template
-	"swSrc": "src/sw-base.js",
-	//Destination where the new service worker will be saved
-	"swDest": "src/custom-sw.js",
-	"globIgnores": [],
-	// Remove 'public/' prefix from url
-	modifyUrlPrefix: {
-		'public/': ''
-	}
+    //Global directory of app
+    "globDirectory": ".",
+    //Patterns for files that will be added
+    "globPatterns": [
+        //Add all assets images
+        "public/manifest.json",
+        "**/js/**\/main.*.js",
+        "**/css/**\/main.*.css",
+        "public/assets/**\/*.{png,jpg,svg}",
+        "**/media/**\/*.{png,jpg,svg}",
+
+
+
+    ],
+    //Destination to service worker template
+    "swSrc": "src/sw-base.js",
+    //Destination where the new service worker will be saved
+    "swDest": "src/custom-sw.js",
+    "globIgnores": [],
+    // Remove 'public/' prefix from url
+    modifyUrlPrefix: {
+        'public/': '',
+        'build/': '',
+    }
 };
