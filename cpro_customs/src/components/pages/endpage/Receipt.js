9ce82f0ab29103708b247329f73d06d49bd4e476
tsm121 <tsm121@gmail.com>
2018-11-12 11:41:42 +0100

    Also fixed indents for good measure 🙃

--- cpro_customs/src/components/pages/endpage/Receipt.js
+++ cpro_customs/src/components/pages/endpage/Receipt.js
@@ -6,29 +6,29 @@ import QRCode from "qrcode.react"
 export default class Receipt extends Component {
 
 
-	render = () => {
-		const {QR_url} = this.props
-		return(
-			<Grid container
-				  direction={"column"}
-				  justify={"center"}
-				  alignItems={"center"}
-			>
-				<Grid item>
-					<h3 className={"modal_title"}>
-						Declaration receipt:
-					</h3>
-				</Grid>
+    render = () => {
+        const {QR_url} = this.props
+        return(
+            <Grid container
+                  direction={"column"}
+                  justify={"center"}
+                  alignItems={"center"}
+            >
+                <Grid item>
+                    <h3 className={"modal_title"}>
+                        Declaration receipt:
+                    </h3>
+                </Grid>
 
-				<Grid item>
-					<QRCode
+                <Grid item>
+                    <QRCode
                         value={QR_url}
                         renderAs="svg"
                         size="256"
                         level="M"
                     />
-				</Grid>
-			</Grid>
-		)
-	}
+                </Grid>
+            </Grid>
+        )
+    }
 }
